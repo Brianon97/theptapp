cd22b2c0c87099622ed6942b98a5a85af4662640
Brianon97 <brianon1997@gmail.com>
2025-12-01 18:00:16 +0000

    update urls for booking

--- trainer/urls.py
+++ trainer/urls.py
@@ -6,7 +6,8 @@ from . import views
 app_name = 'trainer'
 
 urlpatterns = [
-    path('', views.booking_list, name='booking_list'),
+
+    path('bookings/', views.booking_list, name='booking_list'),
     path('new/', views.booking_create, name='booking_create'),
     path('<int:pk>/edit/', views.booking_edit, name='booking_edit'),
     path('<int:pk>/delete/', views.booking_delete, name='booking_delete'),
