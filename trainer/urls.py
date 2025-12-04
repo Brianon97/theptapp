e799d1b67ff0b35767b74569a9b891f993b1f022
Brianon97 <brianon1997@gmail.com>
2025-12-04 17:47:14 +0000

    update model

--- trainer/urls.py
+++ trainer/urls.py
@@ -1,18 +1,18 @@
-# trainer/urls.py  ← Replace with this (cleaner)
-
+# trainer/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'trainer'
 
 urlpatterns = [
-
     path('bookings/', views.booking_list, name='booking_list'),
     path('new/', views.booking_create, name='booking_create'),
     path('<int:pk>/edit/', views.booking_edit, name='booking_edit'),
     path('<int:pk>/delete/', views.booking_delete, name='booking_delete'),
-    path('signup/', views.signup, name='signup'),
-    path('notifications/check/', views.check_notifications, name='check_notifications'),
+    
+    # THIS MUST MATCH THE FUNCTION NAME NOW
     path('client/<int:user_id>/', views.client_detail, name='client_detail'),
     
+    path('signup/', views.signup, name='signup'),
+    path('notifications/check/', views.check_notifications, name='check_notifications'),
 ]
