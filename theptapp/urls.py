b2ba1610d26f6568124d8a645365a7a0f43a3a44
Brianon97 <brianon1997@gmail.com>
2025-12-08 19:16:25 +0000

    fix: include trainer.urls in root urls

--- theptapp/urls.py
+++ theptapp/urls.py
@@ -7,15 +7,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Home page
+    path('', include('trainer.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-
-    # Login / logout
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-
-    # ALL trainer URLs (new/, signup/, etc.)
-    path('', include('trainer.urls', namespace='trainer')),
+    
     
 ]
