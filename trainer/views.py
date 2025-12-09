fee260618d5252c004d263b35d005d2879ea6c2b
Brianon97 <brianon1997@gmail.com>
2025-12-09 12:39:51 +0000

    Fix Heroku deploy: remove broken signals import

--- trainer/views.py
+++ trainer/views.py
@@ -10,9 +10,6 @@ from .models import Booking
 from .forms import BookingForm
 from django import forms
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
 
 
 @login_required
