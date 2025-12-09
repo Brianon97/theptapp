6fd5693a886f91354aead7e2d55792faa1cf0ab9
Brianon97 <brianon1997@gmail.com>
2025-12-09 15:15:54 +0000

    updated urls

--- theptapp/urls.py
+++ theptapp/urls.py
@@ -1,16 +1,13 @@
-# theptapp/urls.py  ← Replace the ENTIRE file with this
-
+# theptapp/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
+from trainer.views import CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('trainer.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    
-    
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('', include('trainer.urls')),
 ]
